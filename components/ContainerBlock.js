519b820ba3619d0f021674a54842e09ddaef2690
Moez Hamza <hamza.moez@gmail.com>
2022-10-19 17:13:52 +0100

    corrected meta tags

--- components/ContainerBlock.js
+++ components/ContainerBlock.js
@@ -20,7 +20,17 @@ export default function ContainerBlock({ children, ...customMeta }) {
     <div>
        <Head>
         <title>{meta.title}</title>
+        <meta name="robots" content="follow, index" />
         <meta content={meta.description} name="description" />
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:site_name" content="Moez Hamza" />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+       
+        {meta.date && (
+          <meta property="article:published_time" content={meta.date} />
+        )}
         </Head>
         <main className="dark:bg-gray-800 w-full">
         <Navbar />
